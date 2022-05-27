611016768c97de96c369637c6b2546cffc99484e
GrandhiM3 <grandhim3@gmail.com>
2022-05-27 19:29:18 +0530

    Updated with case sensitiity

--- Day2/PrintDayNumberbyName.py
+++ Day2/PrintDayNumberbyName.py
@@ -1,17 +1,19 @@
 DayName = input("Day Name is:")
-if DayName=="SUNDAY":
+
+
+if DayName.upper()=="SUNDAY":
     print("Day No is : 1")
-elif DayName=="MONDAY":
+elif DayName.upper()=="MONDAY":
     print("Day No is : 2")
-elif DayName=="TUESDAY":
+elif DayName.upper()=="TUESDAY":
     print("Day No is : 3")
-elif DayName=="WEDNESDAY":
+elif DayName.upper()=="WEDNESDAY":
     print("Day No is : 4")
-elif DayName=="THURSDAY":
+elif DayName.upper()=="THURSDAY":
     print("Day No is : 5")
-elif DayName=="FRIDAY":
+elif DayName.upper()=="FRIDAY":
     print("Day No is : 6")
-elif DayName=="SATURDAY":
+elif DayName.upper()=="SATURDAY":
     print("Day No is : 7")
 else:
     print("INVALID DAY NAME PROVIDED")
